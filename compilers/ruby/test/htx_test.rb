6d49d0830347abcdfa13061f031e140f3aed4a18
Nate Pickens <nate@pickens.email>
2019-07-04 13:43:25 +0700

    Switch Ruby compiler test syntax to Minitest's DSL

--- compilers/ruby/test/htx_test.rb
+++ compilers/ruby/test/htx_test.rb
@@ -1,37 +1,39 @@
 class HTXTest < Minitest::Test
   # NOTE: More granular tests are forthcoming.
 
-  def test_general
-    template_name = '/components/people.htx'
-    template_content = <<~EOS
-      <div class='people'>
-        <h1>${this.title}</h1>
+  describe('HTX.compile') do
+    it('compiles a template') do
+      template_name = '/components/people.htx'
+      template_content = <<~EOS
+        <div class='people'>
+          <h1>${this.title}</h1>
 
-        <ul class='people-list'>
-          for (let person of this.people) {
-            <li class='person ${person.role}'>
-              ${person.name}
-            </li>
-          }
-        </ul>
-      </div>
-    EOS
-
-    compiled = <<~EOS
-      window['/components/people.htx'] = function(htx) {
-        htx.node('div', 'class', `people`, 4)
-          htx.node('h1', 8); htx.node(this.title, 14); htx.close()
-
-          htx.node('ul', 'class', `people-list`, 16)
+          <ul class='people-list'>
             for (let person of this.people) {
-              htx.node('li', 'class', `person ${person.role}`, 20)
-                htx.node(person.name, 26)
-              htx.close()
+              <li class='person ${person.role}'>
+                ${person.name}
+              </li>
             }
-        htx.close(2)
-      }
-    EOS
+          </ul>
+        </div>
+      EOS
 
-    assert_equal(HTX.compile(template_name, template_content), compiled)
+      compiled = <<~EOS
+        window['/components/people.htx'] = function(htx) {
+          htx.node('div', 'class', `people`, 4)
+            htx.node('h1', 8); htx.node(this.title, 14); htx.close()
+
+            htx.node('ul', 'class', `people-list`, 16)
+              for (let person of this.people) {
+                htx.node('li', 'class', `person ${person.role}`, 20)
+                  htx.node(person.name, 26)
+                htx.close()
+              }
+          htx.close(2)
+        }
+      EOS
+
+      HTX.compile(template_name, template_content).must_equal(compiled)
+    end
   end
 end
